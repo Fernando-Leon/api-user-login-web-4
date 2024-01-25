b95e1d46d0b1ae82e3aca240c5b4a84e19c9da63
Fernando-Leon <leon.monroy.fernando@gmail.com>
2024-01-24 20:49:37 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const cors = require('cors');
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+require("dotenv").config();
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());;
+app.use(bodyParser.json());
+const port = process.env.PORT || 9000;
+
+// Middleware
+
+app.use(express.json());
+app.use('/api', userRoutes);
+app.use('/api', authRoutes);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+//MongoDB connection
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err))
+
+app.listen(port, () => {
+    console.log("Server is running on port", port)
+});
